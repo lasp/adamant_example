0d8fb3dc206736d311f2fa3f61ff49718feed6d2
Jacob Seman <90524106+Jbsco@users.noreply.github.com>
2023-11-28 10:09:42 -0700

    Update create_container.sh
    special casing for windows systems and winpty prefix not needed so long as Docker CLI is on path

--- docker/create_container.sh
+++ docker/create_container.sh
@@ -11,11 +11,7 @@ execute "docker run -d \
 
 # Run docker provision script inside of container to get things set up:
 echo "Provisioning container..."
-if [ -n "$ON_WINDOWS" ]; then
-  execute "winpty docker exec -u user $DOCKER_CONTAINER_NAME //share//adamant_example//docker//env//provision//provision_container.sh"
-else
-  execute "docker exec -u user $DOCKER_CONTAINER_NAME /share/adamant_example/docker/env/provision/provision_container.sh"
-fi
+execute "docker exec -u user $DOCKER_CONTAINER_NAME //share//adamant_example//docker//env//provision//provision_container.sh"
 
 echo "Finished creating container \"$DOCKER_CONTAINER_NAME\"."
 execute "docker ps -a"
