a14905008146642ea13e6011b368e365087bdfa8
Jacob Seman <90524106+Jbsco@users.noreply.github.com>
2023-11-28 14:54:46 -0700

    Update create_container.sh
    Relating to comments in issue lasp#2, added case for `linux-gnu` OSTYPE to implement the internal host when creating the docker container on this platform.

--- docker/create_container.sh
+++ docker/create_container.sh
@@ -3,11 +3,20 @@
 # Create the docker container with a bind mount:
 echo "Creating container..."
 . ./docker_config.sh
-execute "docker run -d \
-  --name $DOCKER_CONTAINER_NAME \
-  --mount type=bind,source=\"$(pwd)\"/../..,target=/share \
-  $DOCKER_IMAGE_NAME \
-  sleep infinity"
+if [ -n "ON_LINUX" ]; then
+  execute "docker run -d \
+    --name $DOCKER_CONTAINER_NAME \
+    --mount type=bind,source=\"$(pwd)\"/../..,target=/share \
+    --add-host=host.docker.internal:host-gateway \
+    $DOCKER_IMAGE_NAME \
+    sleep infinity"
+else
+  execute "docker run -d \
+    --name $DOCKER_CONTAINER_NAME \
+    --mount type=bind,source=\"$(pwd)\"/../..,target=/share \
+    $DOCKER_IMAGE_NAME \
+    sleep infinity"
+fi
 
 # Run docker provision script inside of container to get things set up:
 echo "Provisioning container..."
