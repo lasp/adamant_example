cca761fc43f4e2aecb83c8b6d136debfdf0f3b5b
Jacob Seman <90524106+Jbsco@users.noreply.github.com>
2023-11-29 12:23:12 -0700

    Update create_container.sh
    Rather than setting flag, set variable to internal host flag directly.

--- docker/create_container.sh
+++ docker/create_container.sh
@@ -3,20 +3,12 @@
 # Create the docker container with a bind mount:
 echo "Creating container..."
 . ./docker_config.sh
-if [ -n "ON_LINUX" ]; then
-  execute "docker run -d \
-    --name $DOCKER_CONTAINER_NAME \
-    --mount type=bind,source=\"$(pwd)\"/../..,target=/share \
-    --add-host=host.docker.internal:host-gateway \
-    $DOCKER_IMAGE_NAME \
-    sleep infinity"
-else
-  execute "docker run -d \
-    --name $DOCKER_CONTAINER_NAME \
-    --mount type=bind,source=\"$(pwd)\"/../..,target=/share \
-    $DOCKER_IMAGE_NAME \
-    sleep infinity"
-fi
+execute "docker run -d \
+  --name $DOCKER_CONTAINER_NAME \
+  --mount type=bind,source=\"$(pwd)\"/../..,target=/share \
+  $ON_LINUX \
+  $DOCKER_IMAGE_NAME \
+  sleep infinity"
 
 # Run docker provision script inside of container to get things set up:
 echo "Provisioning container..."
