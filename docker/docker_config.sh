10678ce20a51a5bd7d834b1c177d922e1a511bc8
Jacob Seman <90524106+Jbsco@users.noreply.github.com>
2023-11-29 20:20:02 -0700

    Update docker_config.sh
    Moved Linux case &  Docker run flag to `create_container.sh`

--- docker/docker_config.sh
+++ docker/docker_config.sh
@@ -5,14 +5,6 @@ DOCKER_IMAGE_NAME="ghcr.io/lasp/adamant:example-latest"
 export DOCKER_CONTAINER_NAME
 export DOCKER_IMAGE_NAME
 
-ON_LINUX=""
-case "$OSTYPE" in
-  linux*)
-    ON_LINUX="--add-host=host.docker.internal:host-gateway"
-    ;;
-esac
-export ON_LINUX
-
 # Helper function to print out command as executed:
 execute () {
   echo "$ $@"
