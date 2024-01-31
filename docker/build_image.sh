d95114003820c13d3976eda6cfa67cc4c6cf27ca
Kevin Dinkel <1225857+dinkelk@users.noreply.github.com>
2024-01-31 14:12:42 -0700

    cleaning up build_image script

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -1,8 +1,4 @@
 #!/bin/sh -e
 
 . ./docker_config.sh
-# Execute the docker machine from the project/ directory so that we have access
-# to files in both adamant/ and adamant_example/.
-cd ../..
-execute "docker build --progress=plain -t $DOCKER_IMAGE_NAME -f adamant_example/docker/Dockerfile ."
-cd - >/dev/null
+execute "docker build --progress=plain -t $DOCKER_IMAGE_NAME -f Dockerfile ."
