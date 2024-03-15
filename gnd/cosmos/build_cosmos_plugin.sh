4bc7a9af5b592652abadec76a48c9a0a0ae99fde
Jbsco <90524106+Jbsco@users.noreply.github.com>
2024-03-15 16:16:44 -0600

    Update the COSMOS helper script to take the Adamant assembly & COSMOS install location as arguments
    Update the COSMOS script to handle variation in plugin directory case & hyphenation

--- gnd/cosmos/build_cosmos_plugin.sh
+++ gnd/cosmos/build_cosmos_plugin.sh
@@ -7,6 +7,6 @@
 assembly_dir=$1
 # Build all cosmos plugin command and telemetry files:
 cd $assembly_dir
-plugin=`redo what 2>&1 | grep "cosmos/plugin" | awk '{ print $2 }'`
+plugin=`redo what 2>&1 | grep "cosmos/plugin/" | awk '{ print $2 }'`
 echo $plugin | xargs redo-ifchange
 cd - >/dev/null
--- gnd/cosmos/install_cosmos_plugin.sh
+++ gnd/cosmos/install_cosmos_plugin.sh
@@ -4,14 +4,15 @@
 # Copy COSMOS plugin configuration to argument directory
 # This will likely be moved to the Adamant repository
 # COSMOS install directory argument should be relative to the Linux example path
-# Ex. `./install_cosmos_plugin.sh cosmos-project` if COSMOS is adjacent to adamant_example
+# Ex. `./install_cosmos_plugin.sh linux_example cosmos-project` if COSMOS is adjacent to adamant_example
 
-cosmos_install_name=$1
+adamant_assembly_name=$1
+cosmos_install_name=$2
 cosmos_install_dir=`realpath ../../../$cosmos_install_name`
-cosmos_plugin_dir=`realpath $cosmos_install_dir/openc3-cosmos-linux-example`
+cosmos_plugin_dir=`realpath $cosmos_install_dir/openc3-cosmos-${adamant_assembly_name//_/-}`
 adamant_example_plugin_dir=`realpath ../../src/assembly/linux/build/cosmos/plugin`
 adamant_protocol_dir=`realpath ../../../adamant/gnd/cosmos`
-echo cp $adamant_example_plugin_dir/linux_example_ccsds_cosmos_commands.txt $cosmos_plugin_dir/targets/LINUX_EXAMPLE/cmd_tlm/cmd.txt
-echo cp $adamant_example_plugin_dir/linux_example_ccsds_cosmos_telemetry.txt $cosmos_plugin_dir/targets/LINUX_EXAMPLE/cmd_tlm/tlm.txt
-echo cp $adamant_example_plugin_dir/linux_example_ccsds_cosmos_plugin.txt $cosmos_plugin_dir/plugin.txt
-echo cp $adamant_protocol_dir/cmd_checksum.rb $plugin_dir/targets/LINUX_EXAMPLE/lib/cmd_checksum.rb
+echo cp $adamant_example_plugin_dir/${adamant_assembly_name}_ccsds_cosmos_commands.txt $cosmos_plugin_dir/targets/${adamant_assembly_name^^}/cmd_tlm/cmd.txt
+echo cp $adamant_example_plugin_dir/${adamant_assembly_name}_ccsds_cosmos_telemetry.txt $cosmos_plugin_dir/targets/${adamant_assembly_name^^}/cmd_tlm/tlm.txt
+echo cp $adamant_example_plugin_dir/${adamant_assembly_name}_ccsds_cosmos_plugin.txt $cosmos_plugin_dir/plugin.txt
+echo cp $adamant_protocol_dir/cmd_checksum.rb $cosmos_plugin_dir/targets/${adamant_assembly_name^^}/lib/cmd_checksum.rb
