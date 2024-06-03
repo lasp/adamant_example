56ccc71f72f9275edad663db0c5bbd525b8d8a14
Jacob Seman <90524106+Jbsco@users.noreply.github.com>
2024-06-03 12:14:58 -0600

    Add singular script `adamant_env.sh` to handle container creation
    Convert Docker run script to Docker Compose (`docker compose up -d`)
    
    Make `adamant_env.sh` executable
    
    Add all usage args to `adamant_env.sh` output when run with no args
    
    Reword `adamant_env.sh` output to be more approachable to users
    
    Change `stop` arg to stop the container rather than remove
    
    Remove scripts replaced by `adamant_env.sh`
    
    Retain and use `docker_config.sh` for user configuration
    
    Add Docker Compose network mode for changes in #24
    
    Apply `compose.yaml` fixes for Windows
    
    Rename compose.yaml->compose.yml
    
    Update compose.yml to use docker_config.sh vars
    
    Improve `adamant_env.sh` input handling
    
    Convert remaining `docker` commands to `docker-compose`
    
    Fix script `build` arg to function with `docker-compose`
    
    Apply fix to to enable `build` and `push`

---
+++ docker/adamant_env.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+set +e
+
+if ! command -v docker &> /dev/null
+then
+  if command -v podman &> /dev/null
+  then
+    function docker() {
+      podman $@
+    }
+  else
+    echo "Neither docker nor podman found!!!"
+    exit 1
+  fi
+fi
+
+set -e
+
+. ${this_dir}/docker_config.sh
+
+DOCKER_COMPOSE_COMMAND="docker compose"
+DOCKER_COMPOSE_CONFIG="${this_dir}/compose.yml"
+export DOCKER_COMPOSE_COMMAND
+${DOCKER_COMPOSE_COMMAND} version &> /dev/null
+if [ "$?" -ne 0 ]; then
+  export DOCKER_COMPOSE_COMMAND="docker-compose"
+fi
+
+usage() {
+  echo "Usage: $1 [start, stop, login, push, build, remove]" >&2
+  echo "*  start: create and start the adamant_example container" >&2
+  echo "*  stop: stop the running adamant_example container" >&2
+  echo "*  login: login to the adamant_example container" >&2
+  echo "*  push: push the image to the Docker registry" >&2
+  echo "*  build: build the image from the Dockerfile" >&2
+  echo "*  remove: remove network and volumes for adamant_example" >&2
+  exit 1
+}
+
+case $1 in
+  start )
+    execute "${DOCKER_COMPOSE_COMMAND} -f compose.yml up -d"
+    echo ""
+    echo "Run \"./adamant_env.sh login\" to log in."
+    ;;
+  stop )
+    execute "${DOCKER_COMPOSE_COMMAND} -f compose.yml stop"
+    ;;
+  login )
+    execute "${DOCKER_COMPOSE_COMMAND} -f ${DOCKER_COMPOSE_CONFIG} exec -it -u user ${PROJECT_NAME} //bin//bash"
+    ;;
+  push )
+    execute "${DOCKER_COMPOSE_COMMAND} -f ${DOCKER_COMPOSE_CONFIG} push"
+    ;;
+  build )
+    execute "${DOCKER_COMPOSE_COMMAND} -f ${DOCKER_COMPOSE_CONFIG} build"
+    ;;
+  remove )
+    if [ "$2" == "force" ]
+    then
+      execute "${DOCKER_COMPOSE_COMMAND} -f compose.yml down -t 30 -v"
+    else
+      echo "Are you sure? This removes ALL docker volumes and all Adamant Example data! (1-Yes / 2-No)"
+      select yn in "Yes" "No"; do
+        case $yn in
+          Yes ) execute "${DOCKER_COMPOSE_COMMAND} -f compose.yml down -t 30 -v"; break;;
+          No ) exit;;
+        esac
+      done
+    fi
+    ;;
+  * )
+    usage $0
+    ;;
+esac
