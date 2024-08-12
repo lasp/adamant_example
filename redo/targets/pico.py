61195af4dc0ff62e797e7468130c6a6c9a5f1c60
Kevin Dinkel <1225857+dinkelk@users.noreply.github.com>
2024-08-12 13:52:24 -0600

    Add Analyze and Safe build targets for Pico
    The Analyze target is meant to be run with GNAT SAS on a CI server,
    since it forces deep mode analysis. The Safe target is meant to provide
    a "safe image" compile that has runtime checking and optimization turned
    off. This can work around compile bugs or runtime check bugs in a last
    resort image.

--- redo/targets/pico.py
+++ redo/targets/pico.py
@@ -66,3 +66,42 @@ class Pico_Debug(Pico_Base):
             os.environ["EXAMPLE_DIR"],
             "redo" + os.sep + "targets" + os.sep + "gpr" + os.sep + "pico_debug.gpr",
         )
+
+
+class Pico_Safe(Pico_Base):
+    def description(self):
+        return """This target compiles for the Raspberry Pi Pico microprocessor. This is the
+               last resort image compilation. It has all runtime checks and assertions
+               disabled. It has optimization disabled. This is meant to create a different
+               binary than the Pico_Production target in order to possibly circumvent any compiler
+               bugs or runtime check bugs."""
+
+    def gpr_project_file(self):
+        return os.path.join(
+            os.environ["EXAMPLE_DIR"],
+            "redo"
+            + os.sep
+            + "targets"
+            + os.sep
+            + "gpr"
+            + os.sep
+            + "pico_safe.gpr",
+        )
+
+
+class Pico_Analyze(Pico_Base):
+    """Analyze target which runs GNAT SAS in deep mode."""
+    def description(self):
+        return ("Same as Pico_Debug except it adds deep mode switch for GNAT SAS.")
+
+    def gpr_project_file(self):
+        return os.path.join(
+            os.environ["EXAMPLE_DIR"],
+            "redo"
+            + os.sep
+            + "targets"
+            + os.sep
+            + "gpr"
+            + os.sep
+            + "pico_analyze.gpr",
+        )
