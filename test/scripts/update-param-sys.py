eea01f3ad1692bc7672309fc491cc640fba50f0d
Jacob Seman <90524106+Jbsco@users.noreply.github.com>
2024-08-21 18:41:41 -0600

    Include `Update_Parameter_Table` in COSMOS parameter system tests
    squash this

--- test/scripts/update-param-sys.py
+++ test/scripts/update-param-sys.py
@@ -30,9 +30,9 @@ check_formatted("Linux_Example Software_Status_Packet Command_Router_Instance.No
 check_formatted("Linux_Example Software_Status_Packet Command_Router_Instance.Last_Failed_Command.Status == 'FAILURE'")
 print("Noop_Arg last Value failure OK")
 
-#############################################################################
-# If this point was reached then begin testing parameter validation functions
-#############################################################################
+#########################################################################
+# If this point was reached then begin testing parameter update functions
+#########################################################################
 
 # Build a parameter table using bytearrays:
 # Osc_A_Freq
@@ -54,79 +54,44 @@ Test_Packed_Table += bytearray(struct.pack(">h", 0))
 # Buffer_Length
 Test_Packed_Table += bytearray(struct.pack(">h", 55))
 
-# Send nominal Validate_Parameter_Table command expecting success:
-cmd("Linux_Example", "Parameter_Manager_Instance-Validate_Parameter_Table", {
+# Send nominal Update_Parameter_Table command expecting success:
+cmd("Linux_Example", "Parameter_Manager_Instance-Update_Parameter_Table", {
       "Header.Table_Buffer_Length": 24,
       "Header.Crc_Table": 19692,
       "Table_Buffer": list(Test_Packed_Table)
    })
 wait(1.0)
-# Check successful command count is 3 and that it was Validate_Parameter_Table:
+# Check successful command count is 3 and that it was Update_Parameter_Table:
 check_formatted("Linux_Example Software_Status_Packet Command_Router_Instance.Command_Success_Count.Value == '3'")
-check_formatted("Linux_Example Software_Status_Packet Command_Router_Instance.Last_Successful_Command.Id == '5201'")
-print("Validate_Parameter_Table success OK")
+check_formatted("Linux_Example Software_Status_Packet Command_Router_Instance.Last_Successful_Command.Id == '5200'")
+print("Update_Parameter_Table success OK")
 
-# Send test Validate_Parameter_Table command with bad CRC expecting Memory_Region_Crc_Invalid, Table_Validation_Failure, and
-# Command_Execution_Failure:
-cmd("Linux_Example", "Parameter_Manager_Instance-Validate_Parameter_Table", {
+# Send test Update_Parameter_Table command with bad CRC expecting Memory_Region_Crc_Invalid, Parameter_Table_Copy_Failure,
+# Working_Table_Update_Failure, and Command_Execution_Failure:
+cmd("Linux_Example", "Parameter_Manager_Instance-Update_Parameter_Table", {
       "Header.Table_Buffer_Length": 24,
       "Header.Crc_Table": 0,
       "Table_Buffer": list(Test_Packed_Table)
    })
 wait(1.0)
-# Check last failed command count was 2 and that it was Validate_Parameter_Table with Status FAILURE:
+# Check last failed command count was 2 and that it was Update_Parameter_Table with Status FAILURE:
 check_formatted("Linux_Example Software_Status_Packet Command_Router_Instance.Command_Failure_Count.Value == '2'")
-check_formatted("Linux_Example Software_Status_Packet Command_Router_Instance.Last_Failed_Command.ID == '5201'")
-print("Validate_Parameter_Table failure OK")
+check_formatted("Linux_Example Software_Status_Packet Command_Router_Instance.Last_Failed_Command.ID == '5200'")
+print("Update_Parameter_Table failure OK")
 check_formatted("Linux_Example Software_Status_Packet Command_Router_Instance.Last_Failed_Command.Status == 'FAILURE'")
-print("Validate_Parameter_Table last Status FAILURE OK")
+print("Update_Parameter_Table last Status FAILURE OK")
 
-# Send test Validate_Parameter_Table command with bad length expecting Memory_Region_Length_Mismatch, Table_Validation_Failure, and
-# Command_Execution_Failure:
-cmd("Linux_Example", "Parameter_Manager_Instance-Validate_Parameter_Table", {
+# Send test Update_Parameter_Table command with bad length expecting Memory_Region_Length_Mismatch, Parameter_Table_Copy_Failure,
+# Working_Table_Update_Failure, and Command_Execution_Failure:
+cmd("Linux_Example", "Parameter_Manager_Instance-Update_Parameter_Table", {
       "Header.Table_Buffer_Length": 26,
       "Header.Crc_Table": 19692,
       "Table_Buffer": list(Test_Packed_Table)
    })
 wait(1.0)
-# Check last failed command count was 3 and that it was Validate_Parameter_Table with Status FAILURE:
+# Check last failed command count was 3 and that it was Update_Parameter_Table with Status FAILURE:
 check_formatted("Linux_Example Software_Status_Packet Command_Router_Instance.Command_Failure_Count.Value == '3'")
-check_formatted("Linux_Example Software_Status_Packet Command_Router_Instance.Last_Failed_Command.ID == '5201'")
-print("Validate_Parameter_Table failure OK")
+check_formatted("Linux_Example Software_Status_Packet Command_Router_Instance.Last_Failed_Command.ID == '5200'")
+print("Update_Parameter_Table failure OK")
 check_formatted("Linux_Example Software_Status_Packet Command_Router_Instance.Last_Failed_Command.Status == 'FAILURE'")
-print("Validate_Parameter_Table last Status FAILURE OK")
-
-# Send test Validate_Parameter_Table command with nominal values, and Osc_A_Freq = 999.0 to trigger the user implemented Validate_Parameters
-# function, expecting Parameter_Validation_Failed, Parameter_Table_Copy_Failure, Table_Validation_Failure, and Command_Execution_Failure
-# Rebuild the parameter table using known invalid Osc_A_Freq value:
-# Osc_A_Freq
-Test_Packed_Table = bytearray(struct.pack(">f", 999.0))
-# Osc_A_Amp
-Test_Packed_Table += bytearray(struct.pack(">f", 5.00))
-# Osc_A_Off
-Test_Packed_Table += bytearray(struct.pack(">f", 2.50))
-# Osc_B_Freq
-Test_Packed_Table += bytearray(struct.pack(">f", 0.30))
-# Osc_B_Amp
-Test_Packed_Table += bytearray(struct.pack(">f", -5.00))
-# Osc_B_Off
-Test_Packed_Table += bytearray(struct.pack(">f", -2.50))
-# Version
-Test_Packed_Table += bytearray(struct.pack(">f", 0.0))
-# Crc_Table
-Test_Packed_Table += bytearray(struct.pack(">h", 0))
-# Buffer_Length
-Test_Packed_Table += bytearray(struct.pack(">h", 55))
-
-cmd("Linux_Example", "Parameter_Manager_Instance-Validate_Parameter_Table", {
-      "Header.Table_Buffer_Length": 24,
-      "Header.Crc_Table": 65043,
-      "Table_Buffer": list(Test_Packed_Table)
-   })
-wait(1.0)
-# Check last failed command count was 3 and that it was Validate_Parameter_Table with Status FAILURE:
-check_formatted("Linux_Example Software_Status_Packet Command_Router_Instance.Command_Failure_Count.Value == '4'")
-check_formatted("Linux_Example Software_Status_Packet Command_Router_Instance.Last_Failed_Command.ID == '5201'")
-print("Validate_Parameter_Table failure OK")
-check_formatted("Linux_Example Software_Status_Packet Command_Router_Instance.Last_Failed_Command.Status == 'FAILURE'")
-print("Validate_Parameter_Table last Status FAILURE OK")
+print("Update_Parameter_Table last Status FAILURE OK")
