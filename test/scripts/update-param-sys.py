9b73651d0a7265a54769f16fb3f1d8bb6107d659
Jacob Seman <90524106+Jbsco@users.noreply.github.com>
2024-08-26 14:28:59 -0600

    Switch testing from `check_formatted` to `wait_check`

--- test/scripts/update-param-sys.py
+++ test/scripts/update-param-sys.py
@@ -5,29 +5,27 @@ from openc3.script import *
 import struct
 
 # Clear counts and data products from prior tests:
+cmd("Linux_Example Fault_Correction_Instance-Reset_Data_Products")
 cmd("Linux_Example Ccsds_Command_Depacketizer_Instance-Reset_Counts")
 cmd("Linux_Example Command_Router_Instance-Reset_Data_Products")
-wait(1.0)
 
 # Send Noop_Arg command expecting success:
 cmd("Linux_Example Command_Router_Instance-Noop_Arg with Value 1")
-wait(1.0)
 # Check successful command count is 2 and that the last success was Noop_Arg with last Value 1:
-check_formatted("Linux_Example Software_Status_Packet Command_Router_Instance.Command_Success_Count.Value == '2'")
-check_formatted("Linux_Example Software_Status_Packet Command_Router_Instance.Last_Successful_Command.Id == '3'")
+wait_check("Linux_Example Software_Status_Packet Command_Router_Instance.Command_Success_Count.Value == 2", 3)
+wait_check("Linux_Example Software_Status_Packet Command_Router_Instance.Last_Successful_Command.Id == 3", 3)
 print("Noop_Arg success OK")
-check_formatted("Linux_Example Software_Status_Packet Command_Router_Instance.Noop_Arg_Last_Value.Value == '1'")
+wait_check("Linux_Example Software_Status_Packet Command_Router_Instance.Noop_Arg_Last_Value.Value == 1", 3)
 print("Noop_Arg last Value OK")
 
 # Send Noop command expecting failure:
 cmd("Linux_Example Command_Router_Instance-Noop_Arg with Value 868")
-wait(1.0)
 # Check last failed command count was 1 and that it was Noop_Arg with last Value 1:
-check_formatted("Linux_Example Software_Status_Packet Command_Router_Instance.Command_Failure_Count.Value == '1'")
-check_formatted("Linux_Example Software_Status_Packet Command_Router_Instance.Last_Failed_Command.ID == '3'")
+wait_check("Linux_Example Software_Status_Packet Command_Router_Instance.Command_Failure_Count.Value == 1", 3)
+wait_check("Linux_Example Software_Status_Packet Command_Router_Instance.Last_Failed_Command.ID == 3", 3)
 print("Noop_Arg failure OK")
-check_formatted("Linux_Example Software_Status_Packet Command_Router_Instance.Noop_Arg_Last_Value.Value == '868'")
-check_formatted("Linux_Example Software_Status_Packet Command_Router_Instance.Last_Failed_Command.Status == 'FAILURE'")
+wait_check("Linux_Example Software_Status_Packet Command_Router_Instance.Noop_Arg_Last_Value.Value == 868", 3)
+wait_check("Linux_Example Software_Status_Packet Command_Router_Instance.Last_Failed_Command.Status == 'FAILURE'", 3)
 print("Noop_Arg last Value failure OK")
 
 #########################################################################
@@ -60,10 +58,9 @@ cmd("Linux_Example", "Parameter_Manager_Instance-Update_Parameter_Table", {
       "Header.Crc_Table": 19692,
       "Table_Buffer": list(Test_Packed_Table)
    })
-wait(1.0)
 # Check successful command count is 3 and that it was Update_Parameter_Table:
-check_formatted("Linux_Example Software_Status_Packet Command_Router_Instance.Command_Success_Count.Value == '3'")
-check_formatted("Linux_Example Software_Status_Packet Command_Router_Instance.Last_Successful_Command.Id == '5200'")
+wait_check("Linux_Example Software_Status_Packet Command_Router_Instance.Command_Success_Count.Value == 3", 3)
+wait_check("Linux_Example Software_Status_Packet Command_Router_Instance.Last_Successful_Command.Id == 5200", 3)
 print("Update_Parameter_Table success OK")
 
 # Send test Update_Parameter_Table command with bad CRC expecting Memory_Region_Crc_Invalid, Parameter_Table_Copy_Failure,
@@ -73,12 +70,11 @@ cmd("Linux_Example", "Parameter_Manager_Instance-Update_Parameter_Table", {
       "Header.Crc_Table": 0,
       "Table_Buffer": list(Test_Packed_Table)
    })
-wait(1.0)
 # Check last failed command count was 2 and that it was Update_Parameter_Table with Status FAILURE:
-check_formatted("Linux_Example Software_Status_Packet Command_Router_Instance.Command_Failure_Count.Value == '2'")
-check_formatted("Linux_Example Software_Status_Packet Command_Router_Instance.Last_Failed_Command.ID == '5200'")
+wait_check("Linux_Example Software_Status_Packet Command_Router_Instance.Command_Failure_Count.Value == 2", 3)
+wait_check("Linux_Example Software_Status_Packet Command_Router_Instance.Last_Failed_Command.ID == 5200", 3)
 print("Update_Parameter_Table failure OK")
-check_formatted("Linux_Example Software_Status_Packet Command_Router_Instance.Last_Failed_Command.Status == 'FAILURE'")
+wait_check("Linux_Example Software_Status_Packet Command_Router_Instance.Last_Failed_Command.Status == 'FAILURE'", 3)
 print("Update_Parameter_Table last Status FAILURE OK")
 
 # Send test Update_Parameter_Table command with bad length expecting Memory_Region_Length_Mismatch, Parameter_Table_Copy_Failure,
@@ -88,10 +84,9 @@ cmd("Linux_Example", "Parameter_Manager_Instance-Update_Parameter_Table", {
       "Header.Crc_Table": 19692,
       "Table_Buffer": list(Test_Packed_Table)
    })
-wait(1.0)
 # Check last failed command count was 3 and that it was Update_Parameter_Table with Status FAILURE:
-check_formatted("Linux_Example Software_Status_Packet Command_Router_Instance.Command_Failure_Count.Value == '3'")
-check_formatted("Linux_Example Software_Status_Packet Command_Router_Instance.Last_Failed_Command.ID == '5200'")
+wait_check("Linux_Example Software_Status_Packet Command_Router_Instance.Command_Failure_Count.Value == 3", 3)
+wait_check("Linux_Example Software_Status_Packet Command_Router_Instance.Last_Failed_Command.ID == 5200", 3)
 print("Update_Parameter_Table failure OK")
-check_formatted("Linux_Example Software_Status_Packet Command_Router_Instance.Last_Failed_Command.Status == 'FAILURE'")
+wait_check("Linux_Example Software_Status_Packet Command_Router_Instance.Last_Failed_Command.Status == 'FAILURE'", 3)
 print("Update_Parameter_Table last Status FAILURE OK")
