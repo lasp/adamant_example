7a7f05b63a422a794b79f764557a0104812377b5
Kevin Dinkel <1225857+dinkelk@users.noreply.github.com>
2025-01-31 08:40:45 -0700

    Update env.py files to append to EXTRA_BUILD_PATH
    See
    https://github.com/lasp/adamant/commit/70d18f75842ae26e4b27a80e169fea88752434e1
    for more details on this change in the Adamant repository. This migrates
    the same change to the example.

--- redo/environments/pico.py
+++ redo/environments/pico.py
@@ -1,10 +1,11 @@
 # Import this file to change the default target for a directory to Pico instead of Linux.
+from environments import modify_build_path
 from util import target
-from os import environ, path
+from os import path, environ
 
 target.set_target_if_not_set("Pico")
 try:
     adamant_dir = environ['ADAMANT_DIR']
 except KeyError:
     adamant_dir = '/home/user/adamant'
-environ["REMOVE_BUILD_PATH"] = path.join(adamant_dir, "src/components/ccsds_serial_interface/uart")
+modify_build_path.remove_from_build_path(path.join(adamant_dir, "src/components/ccsds_serial_interface/uart"))
